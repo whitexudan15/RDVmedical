a8f4fff36b1371be874587744997c5806e184739
whitexudan15 <tresortakoudawhitexudan15@gmail.com>
2025-02-06 08:26:59 +0000

    code base Servlets

--- src/main/java/com/rdvmedical/controller/MedecinController.java
+++ src/main/java/com/rdvmedical/controller/MedecinController.java
@@ -1,5 +1,30 @@
 package com.rdvmedical.controller;
 
-public class MedecinController {
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MedecinController
+ */
+@WebServlet("/medecin")
+public class MedecinController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public MedecinController() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
 
 }
--- src/main/java/com/rdvmedical/controller/SecretaireController.java
+++ src/main/java/com/rdvmedical/controller/SecretaireController.java
@@ -1,5 +1,30 @@
 package com.rdvmedical.controller;
 
-public class SecretaireController {
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SecretaireController
+ */
+@WebServlet("/secretaire")
+public class SecretaireController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public SecretaireController() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
 
 }
